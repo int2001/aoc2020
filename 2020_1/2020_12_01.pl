f13eda72357f6ca83d936e66411fadbd488bec88
Jörg Dorgeist <jorgdorgeist@macbook13.dyn.dorgeist.net>
2020-12-06 14:49:59 +0100

    cleaner

--- 2020_1/2020_12_01.pl
+++ 2020_1/2020_12_01.pl
@@ -13,10 +13,7 @@ for (my $i=0;$i<scalar(@d);$i++) {
 	for (my $j=0;$j<scalar(@d);$j++) {
 		$p1="Part 1: ".($d[$i]*$d[$j])."\n" if (($d[$i]+$d[$j]) == 2020);
 		for (my $k=0;$k<scalar(@d);$k++) {
-			if (($d[$i]+$d[$j]+$d[$k]) == 2020) {
-				my $sumup=($d[$i]*$d[$j]*$d[$k]);
-				$p2="Part 2: ".$sumup."\n";
-			}
+			$p2="Part 2: ".($d[$i]*$d[$j]*$d[$k])."\n" if (($d[$i]+$d[$j]+$d[$k]) == 2020);
 		}
 	}
 }
